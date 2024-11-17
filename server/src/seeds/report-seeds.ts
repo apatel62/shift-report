3e2e1083a09c1106b0162dc78afbfebc2130dc62
Keaton Greer <keatongreer1@gmail.com>
2024-11-17 17:36:09 -0500

    Added backend for history queries

--- server/src/seeds/report-seeds.ts
+++ server/src/seeds/report-seeds.ts
@@ -4,13 +4,23 @@ export const seedReports = async () => {
   await Report.bulkCreate([
     {
       shiftNumber: "Shift 1",
-      date: new Date(),
+      date: new Date("2024-11-16"),
       assignedUserId: 1,
     },
     {
       shiftNumber: "Shift 2",
-      date: new Date(),
+      date: new Date("2024-11-16"),
       assignedUserId: 2,
+    },
+    {
+      shiftNumber: "Shift 1",
+      date: new Date(),
+      assignedUserId: 3,
+    },
+    {
+      shiftNumber: "Shift 2",
+      date: new Date(),
+      assignedUserId: 1,
     },
   ]);
 };
